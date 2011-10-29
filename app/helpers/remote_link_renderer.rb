f48f24f4aa67cf520273e4cf90567ea88dd5f75c
unknown <coorasse@.(none)>
2011-10-29 14:04:32 +0200

    changed class

--- app/helpers/remote_link_renderer.rb
+++ app/helpers/remote_link_renderer.rb
@@ -1,4 +1,4 @@
-class RemoteLinkRenderer < WillPaginate::LinkRenderer
+class RemoteLinkRenderer < WillPaginate::ViewHelpers::LinkRenderer
   def initialize(collection, options, template)
     @remote = options.delete(:remote)
     super
