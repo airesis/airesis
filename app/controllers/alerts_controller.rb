252f3d7a917cb70dd0d3791acf651795d33072ea
coorasse <coorasse@gmail.com>
2012-01-14 22:11:59 +0100

    mostra gli alert dell'utente

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -3,4 +3,10 @@ class AlertsController < ApplicationController
 
   before_filter :authenticate_user!
 
+  #mostra gli alert dell'utente corrente
+  def index
+    @new_user_alerts = current_user.user_alerts.find(:all, :include => :notification, :conditions => 'checked = false')
+    @old_user_alerts = current_user.user_alerts.find(:all, :include => :notification, :conditions => 'checked = true')
+  end
+  
 end  
