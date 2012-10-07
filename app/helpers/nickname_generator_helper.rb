885236fb20b3c1a0a06837a2b4b2f965afdf47ed
coorasse <coorasse@gmail.com>
2012-10-07 23:57:41 +0200

    mostro il nome reale dell'utente connesso

--- app/helpers/nickname_generator_helper.rb
+++ app/helpers/nickname_generator_helper.rb
@@ -9,7 +9,7 @@ module NicknameGeneratorHelper
   def self.give_me_a_nickname
     name_generator = NameGenerator.new(@data_handler.follower_letters)
     names = name_generator.generate_names(@data_handler.start_pairs, 1)
-    return names[0]
+    return names[0].strip
    end
 
 end
