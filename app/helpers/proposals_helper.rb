05baf8aff7a773d72798c39952bf265c23e78b6f
Alessandro Rodi <coorasse@gmail.com>
2012-12-07 17:57:44 +0100

    helper!

---
+++ app/helpers/proposals_helper.rb
@@ -0,0 +1,31 @@
+module ProposalsHelper
+  
+  #verifica se è necessario passare alla fase di votazione
+  #una proposta attualmente in fase di valutazione e dibattito
+  def check_phase(proposal)
+    quorum = proposal.quorum
+    passed = false
+    timepassed = (!quorum.ends_at || Time.now > quorum.ends_at)
+    vspassed = (!quorum.valutations || proposal.valutations >= quorum.valutations)
+    #se erano definiti entrambi i parametri
+    if (quorum.ends_at && quorum.valutations)
+      if (quorum.or?)
+        passed = (timepassed || vpassed)
+      else (quorum.and?)
+        passed = (timepassed && vpassed)
+      end
+    else #altrimenti era definito solo uno dei due, una delle due variabili 
+      passed = (timepassed && vpassed)
+    end
+    
+    if (passed)
+      if (proposal.rank >= quorum.good_score)
+        proposal.proposal_state_id = PROP_WAIT_DATE  #metti la proposta in attesa di una data per la votazione
+        notify_proposal_ready_for_vote(proposal)
+      else if (proposal.rank < quorum.bad_score)
+        proposal.proposal_state_id = PROP_RESP
+        notify_proposal_rejected(proposal)
+      end
+    end
+  end
+end
