7ea611c374af640833998fb335410ced8ae6d0e9
Alessandro Rodi <coorasse@gmail.com>
2013-01-16 12:38:11 +0100

    aggiunta funzione javascript per tooltip

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,11 @@
 
 FACEBOOK_APP_ID="221145254619152"
+FACEBOOK_APP_SECRET=""
 GOOGLE_APP_ID="597462824491.apps.googleusercontent.com"
-
+GOOGLE_APP_SECRET=""
+EMAIL_ADDRESS=""
+EMAIL_USERNAME=""
+EMAIL_PASSWORD=""
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
