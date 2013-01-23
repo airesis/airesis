4c07fb2fc7c31131687fcfc3bf5d9c64eba6434f
Alessandro Rodi <coorasse@gmail.com>
2013-01-23 12:48:24 +0100

    other fixes for assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,9 +15,9 @@ DemocracyOnline3::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
-  config.assets.precompile += %w(endless_page.js back_enabled.png)
+  config.assets.precompile += %w(endless_page.js back_enabled.png landing.css redmond/custom.css)
 
 
   # Generate digests for assets URLs
