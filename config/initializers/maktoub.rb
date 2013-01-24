9b083ecc1ad6e8b37276824c5b6e7b60d0dac3a6
coorasse <coorasse@gmail.com>
2013-01-24 21:26:55 +0100

    facebook url

--- config/initializers/maktoub.rb
+++ config/initializers/maktoub.rb
@@ -1,7 +1,7 @@
 #Maktoub.from = "Airesis Newsletter <noreply@airesis.it>" # the email the newsletter is sent from
 Maktoub.from = "Coorasse <coorasse@gmail.com>" # the email the newsletter is sent from
 Maktoub.twitter_url = "http://twitter.com/#!/democracyo" # your twitter page
-Maktoub.facebook_url = "http://www.facebook.com/facebook" # your facebook oage
+Maktoub.facebook_url = "https://www.facebook.com/democracyonline" # your facebook oage
 Maktoub.google_plus_url = "https://plus.google.com/b/108270340723482080584/108270340723482080584" # your facebook oage
 Maktoub.subscription_preferences_url = "http://www.airesis.it/users/alarm_preferences" #subscribers management url
 Maktoub.logo = "logo120.png" # path to your logo asset
@@ -14,9 +14,7 @@ Maktoub.home_domain = "www.airesis.it" # your domain
 
 # require "ostruct"
  Maktoub.subscribers_extractor do
-   (1..5).map do |i|
-     users << OpenStruct.new({name: "tester#{i}", email: "test#{i}@example.com"})
-  end
+     users = User.all
  end
 
 # uncomment lines below to change subscriber fields that contain email and
