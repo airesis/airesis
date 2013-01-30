0164a30b8b8ab56dbe15e9682434df1a4abcd129
Alessandro Rodi <coorasse@gmail.com>
2013-01-30 12:55:55 +0100

    reconfirmable email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -98,7 +98,7 @@ Devise.setup do |config|
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
   # db field (see migrations). Until confirmed new email is stored in
   # unconfirmed email column, and copied to email column on successful confirmation.
-  #config.reconfirmable = true
+  config.reconfirmable = true
 
   # Defines which key will be used when confirming an account
   # config.confirmation_keys = [ :email ]
@@ -116,7 +116,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length. Default is 6..128.
-  # config.password_length = 6..128
+  config.password_length = 6..128
 
   # Email regex used to validate email formats. It simply asserts that
   # an one (and only one) @ exists in the given string. This is mainly
---
+++ db/migrate/20130130102655_add_reconfirmable_column.rb
@@ -0,0 +1,9 @@
+class AddReconfirmableColumn < ActiveRecord::Migration
+  def up
+    add_column :users, :unconfirmed_email, :string, :limit => 100
+  end
+
+  def down
+    remove_column :users, :unconfirmed_email
+  end
+end
