9287f54bdfeb4c744059776b502331df174189af
coorasse <coorasse@gmail.com>
2013-02-22 14:55:27 +0100

    fix and query performances

---
+++ db/migrate/20130222134529_missing_email_objects.rb
@@ -0,0 +1,16 @@
+class MissingEmailObjects < ActiveRecord::Migration
+  def up
+    n = NotificationType.find_by_id(22)
+    n.email_subject = "Un utente si è reso disponibile a redigere la sintesi di una proposta"
+    n.save!
+    n = NotificationType.find_by_id(23)
+    n.email_subject = "Sei stato scelto come redattore di una proposta"
+    n.save!
+    n = NotificationType.find_by_id(24)
+    n.email_subject = "Nuovi redattori per una proposta"
+    n.save!
+  end
+
+  def down
+  end
+end
