ab6499a193f6c26c6bf39ee658c7ac7568642e0e
coorasse <coorasse@gmail.com>
2013-02-22 13:48:46 +0100

    fixes

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -1,5 +1,5 @@
 class Configuration < ActiveRecord::Base
   def self.phases_active
-      @phases_active = !self.find_by_name('phases_active').value.to_i.zero? unless @phases_active
+      @phases_active = !self.find_by_name('phases_active').value.to_i.zero?
   end
 end
