f7aa7f8b26cf387bdb83eeda605818ad99b6beaa
coorasse <coorasse@gmail.com>
2013-04-19 17:05:04 +0200

    new mail layout

--- app/helpers/notification_helper.rb
+++ app/helpers/notification_helper.rb
@@ -47,7 +47,9 @@ module NotificationHelper
     nickname = ProposalNickname.find_by_user_id_and_proposal_id(comment_user.id,proposal.id)
     name = (nickname && proposal.is_anonima?) ? nickname.nickname : comment_user.fullname #send nickname if proposal is anonymous
     msg = "<b>"+name+"</b> ha inserito un commento alla tua proposta <b>"+proposal.title+"</b>!";
-    data = {'comment_id' => comment.id.to_s, 'proposal_id' => proposal.id.to_s, 'to_id' => "proposal_c_#{proposal.id}"}
+    subject = proposal.private? ? "[#{proposal.presentation_groups.first.name}]" : ''
+    subject +=  " #{proposal.title} - Nuovo contributo"
+    data = {'comment_id' => comment.id.to_s, 'proposal_id' => proposal.id.to_s, 'to_id' => "proposal_c_#{proposal.id}", 'subject' => subject, 'username' => name}
     notification_a = Notification.new(:notification_type_id => 5,:message => msg, :url => proposal_path(proposal) +"#comment"+comment.id.to_s, :data => data)
     notification_a.save
     proposal.users.each do |user|
--- app/mailers/resque_mailer.rb
+++ app/mailers/resque_mailer.rb
@@ -4,14 +4,21 @@ class ResqueMailer < ActionMailer::Base
   default from: "Airesis <info@airesis.it>"
 
   layout 'maktoub/newsletter_mailer'
+
+
+  TEMPLATES = { 5 => 'new_contribute', 1 => 'new_contribute'}
   
   def notification(alert_id)
     @alert = UserAlert.find(alert_id)
-    to_id = @alert.notification.data['to_id']
+    @data = @alert.notification.data
+    to_id = @data['to_id']
+    subject_id = @data['subject']
+    subject = subject_id || @alert.notification.notification_type.email_subject
+    template_name = TEMPLATES[@alert.notification.notification_type_id] || 'notification'
     if to_id
-      mail(:to => "discussion+#{to_id}@airesis.it", :bcc => @alert.user.email, :subject => @alert.notification.notification_type.email_subject)
+      mail(:to => "discussion+#{to_id}@airesis.it", :bcc => @alert.user.email, :subject => subject, :template_name => template_name)
     else
-      mail(:to => @alert.user.email, :subject => @alert.notification.notification_type.email_subject)
+      mail(:to => @alert.user.email, :subject => subject, :template_name => template_name)
     end
   end
   
