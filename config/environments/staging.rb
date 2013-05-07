314276fcf2af85502cebfa1acdc617fb73ee33a6
coorasse <coorasse@gmail.com>
2013-05-07 13:29:49 +0200

    precompile assets

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -15,7 +15,7 @@ DemocracyOnline3::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   config.assets.precompile += %w(endless_page.js back_enabled.png landing.css redmond/custom.css menu_left.css jquery.js ice/index.js html2canvas.js)
 
