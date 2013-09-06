ca44b33c31738b4ee39d30914eee0367031ba3aa
coorasse <coorasse@gmail.com>
2013-09-06 18:27:27 +0200

    i18n of notifications

--- app/controllers/group_partecipations_controller.rb
+++ app/controllers/group_partecipations_controller.rb
@@ -27,9 +27,9 @@ class GroupPartecipationsController < ApplicationController
 
   def build_csv
     CSV.generate do |csv|
-      csv << ['Cognome','Nome','Ruolo','Iscritto dal']
+      csv << ['Cognome', 'Nome', 'Ruolo', 'Iscritto dal']
       @group_partecipations.each do |group_partecipation|
-        csv << [group_partecipation.user.surname,group_partecipation.user.name,group_partecipation.partecipation_role.name,group_partecipation.created_at ? (l group_partecipation.created_at) : ' ']
+        csv << [group_partecipation.user.surname, group_partecipation.user.name, group_partecipation.partecipation_role.name, group_partecipation.created_at ? (l group_partecipation.created_at) : ' ']
       end
     end
   end
@@ -40,24 +40,24 @@ class GroupPartecipationsController < ApplicationController
     ids = params[:message][:receiver_ids]
     subject = params[:message][:subject]
     body = params[:message][:body]
-    ResqueMailer.massive_email(current_user.id,ids,@group.id,subject,body).deliver!
+    ResqueMailer.massive_email(current_user.id, ids, @group.id, subject, body).deliver!
     flash[:notice] = t('info.message_sent')
   end
 
   def destroy
     authorize! :destroy, @group_partecipation
 
-    @group_partecipation_request = GroupPartecipationRequest.find_by_user_id_and_group_id(@group_partecipation.user_id,@group_partecipation.group_id)
+    @group_partecipation_request = GroupPartecipationRequest.find_by_user_id_and_group_id(@group_partecipation.user_id, @group_partecipation.group_id)
 
     if @group_partecipation.partecipation_role_id == PartecipationRole::PORTAVOCE &&
-       @group_partecipation.group.portavoce.count == 1
+        @group_partecipation.group.portavoce.count == 1
       flash[:error] = "Non puoi uscire da un gruppo del quale sei l'unico portavoce"
     else
       GroupPartecipation.transaction do
         @group_partecipation_request.destroy
         @group_partecipation.destroy
       end
-      flash[:notice] = current_user == @group_partecipation.user ? "Sei uscito dal gruppo. In futuro potrai richiedere nuovamente di parteciparvi" : "#{@group_partecipation.user.fullname} "+t('info.participation_roles.user_removed_from_group') #TODO:il18n
+      flash[:notice] = (current_user == @group_partecipation.user) ? "Sei uscito dal gruppo. In futuro potrai richiedere nuovamente di parteciparvi" : "#{@group_partecipation.user.fullname} "+ I18n.t('info.participation_roles.user_removed_from_group') #TODO:il18n
     end
 
     respond_to do |format|
@@ -65,12 +65,12 @@ class GroupPartecipationsController < ApplicationController
       #format.xml  { head :ok }
     end
   end
-  
+
   protected
 
   def load_group_partecipation
     @group_partecipation = GroupPartecipation.find(params[:id])
   end
-  
-  
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130906113428) do
+ActiveRecord::Schema.define(:version => 20130906154044) do
 
   create_table "action_abilitations", :force => true do |t|
     t.integer  "group_action_id"
