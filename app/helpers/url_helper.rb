13767a3e5ab09850f6c3d1943392db44edf17b45
coorasse <coorasse@gmail.com>
2013-09-06 12:55:18 +0200

    another url fix

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -3,7 +3,7 @@ module UrlHelper
   def with_subdomain(subdomain)
     subdomain = (subdomain || "")
     subdomain += "." unless subdomain.empty?
-    subdomain += "www." if (request.subdomain == 'www') || (subdomain.empty? && !request.subdomain.empty?)
+    subdomain += "www." if subdomain.empty? && !request.subdomain.empty?
     host = Rails.application.config.action_mailer.default_url_options[:host]
     host = host.split(':')[0]
     [subdomain, host].join
