a6aa05d44492974d9e12e0338556fe00722fd333
coorasse <coorasse@gmail.com>
2013-09-11 13:15:22 +0200

    urgent fix

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -4,7 +4,7 @@ module UrlHelper
     subdomain = (subdomain || "")
     subdomain += "." unless subdomain.empty?
     subdomain += "www." if subdomain.empty? && !request.subdomain.empty?
-    host = (defined? request) ? request.domain : Rails.application.config.action_mailer.default_url_options[:host]
+    host = ((defined? request) && request) ? request.domain : Rails.application.config.action_mailer.default_url_options[:host]
     host = host.gsub('www.','').split(':')[0]
     [subdomain, host].join
   end
