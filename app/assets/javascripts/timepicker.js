35c03aa5a7b06a5990c35a454fedf0a9f7b1fa66
mion00 <mion00@gmail.com>
2013-10-11 12:02:29 +0200

    I18n improvements time-picker

--- app/assets/javascripts/timepicker.js
+++ app/assets/javascripts/timepicker.js
@@ -33,7 +33,7 @@
             closeText: 'Done',
             amNames: ['AM', 'A'],
             pmNames: ['PM', 'P'],
-            timeFormat: 'HH:mm',
+            timeFormat: 'hh:mm TT',
             timeSuffix: '',
             timeOnlyTitle: 'Choose Time',
             timeText: 'Time',
