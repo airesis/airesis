e779945a068fb17bd1ae26190d87d407f6ce8fc2
Alessandro <coorasse@francesca.(none)>
2013-11-01 14:13:34 +0100

    assets in staging

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'fb_graph'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require *Rails.groups(:assets => %w(development test))
+  Bundler.require *Rails.groups(:assets => %w(development test staging))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
