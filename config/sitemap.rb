7247e24a0733791ae4bbc0d9efa5968a8e8883de
Alessandro <coorasse@francesca.(none)>
2013-11-18 13:19:03 +0100

    new sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -6,6 +6,8 @@ SitemapGenerator::Sitemap.create do
   add proposals_path, changefreq: 'always', priority: 1
   add groups_path, changefreq: 'daily', priority: 0.9
   add edemocracy_path, changefreq: 'monthly', priority: 0.9
+  add eparticipation_path, changefreq: 'monthly', priority: 0.9
+  add story_path, changefreq: 'monthly', priority: 0.9
   add blogs_path, changefreq: 'daily', priority: 0.7
   add events_path, changefreq: 'daily', priority: 0.7
   add '?l=pt', changefreq: 'monthly', priority: 0.8
