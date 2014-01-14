6fb1a0fa2ca682a50d6926e491406b441fae2833
Alessandro Rodi <coorasse@gmail.com>
2014-01-14 15:19:31 +0100

    quick fix for statistics

--- app/workers/calculate_group_statistics.rb
+++ app/workers/calculate_group_statistics.rb
@@ -4,8 +4,8 @@ class CalculateGroupStatistics
     Group.all.each do |group|
       statistic = group.statistic
       proposals = group.internal_proposals.where("proposal_state_id not in (#{ProposalState::VALUTATION})") #take all group proposals currently not in debate
-      statistic.valutations = proposals.average('valutations')
-      statistic.good_score = proposals.average('rank')
+      statistic.valutations = proposals.average('valutations') || 0
+      statistic.good_score = proposals.average('rank') || 0
       vote_participants = 0
       count = 0
       proposals = group.internal_proposals.voted.each do |proposal| #take all group proposals voted (accepted or rejected)
