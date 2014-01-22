973f0754fdc19eb5853d0d31161e1542c68de9a2
Alessandro Rodi <coorasse@gmail.com>
2014-01-22 12:50:36 +0100

    changed development configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,31 +2,33 @@ Airesis::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
-  # every request.  This slows down response time but is perfect for development
+  # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # Do not eager load code on boot.
   config.eager_load = false
 
-  # Show full error reports and disable caching
-  config.consider_all_requests_local       = false
+  # Show full error reports and disable caching.
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Print deprecation notices to the Rails logger
+  # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  config.i18n.fallbacks = true
+  # Raise an error on page load if there are pending migrations
+  config.active_record.migration_error = :page_load
 
-
-  # Expands the lines which load the assets
   config.assets.debug = true
-  
+
+  config.i18n.fallbacks = true
+    
   #config.assets.logger = nil
   config.force_ssl = false
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => MAILER_DEFAULT_HOST }
   
   
