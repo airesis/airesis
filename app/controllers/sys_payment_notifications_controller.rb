e60a5da134b721f0c0476775af0ad4ac254a747a
Alessandro Rodi <coorasse@gmail.com>
2014-01-28 11:29:15 +0100

    fix for non us payments

--- app/controllers/sys_payment_notifications_controller.rb
+++ app/controllers/sys_payment_notifications_controller.rb
@@ -6,7 +6,7 @@ class SysPaymentNotificationsController < ApplicationController
     response = validate_IPN_notification(request.raw_post)
     case response
       when "VERIFIED"
-        SysPaymentNotification.create!(:params => params, :sys_feature_id => (params[:item_number].to_i rescue nil), :status => params[:payment_status], :transaction_id => params[:txn_id], payment_fee: params[:payment_fee], payment_gross: params[:payment_gross], first_name: params[:first_name], last_name: params[:last_name])
+        SysPaymentNotification.create!(:params => params, :sys_feature_id => (params[:item_number].to_i rescue nil), :status => params[:payment_status], :transaction_id => params[:txn_id], payment_fee: params[:mc_fee], payment_gross: params[:mc_gross], first_name: params[:first_name], last_name: params[:last_name])
       when "INVALID"
         log_error Exception.new('invalid ipn received')
       else
