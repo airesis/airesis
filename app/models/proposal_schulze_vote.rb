a0363256c2d09f7cadc6e7e8810403731e891561
Alessandro Rodi <coorasse@gmail.com>
2014-02-05 09:34:34 +0100

    bugfix

--- app/models/proposal_schulze_vote.rb
+++ app/models/proposal_schulze_vote.rb
@@ -6,7 +6,7 @@ class ProposalSchulzeVote < ActiveRecord::Base
     #ids is an array of two. each element with his previous delimiter
     ids = self.preferences.scan(/(;|,|)(\d+)/).map{|d,n| [d,n.to_i]}.each do |d,n|
       desc += (d == ',' ? ' , ' : ' </br>') unless d.empty?
-      desc += Solution.where(:id => n).pluck(:title).first
+      desc += Solution.where(:id => n).pluck(:title).first.to_s #bugfix if no title defined
     end
     desc.html_safe
   end
