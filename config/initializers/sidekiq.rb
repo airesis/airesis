5655d2b3eb94e7d01344872ccaa7b64f3267e004
Alessandro Rodi <coorasse@gmail.com>
2014-06-26 23:00:28 +0200

    moving sidekiq configuration to external file

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379/12'}
+  config.redis = { url: ENV['REDIS_URL']}
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379/12'}
+  config.redis = { url: ENV['REDIS_URL']}
 end
 
 #Resque.schedule = YAML.load_file(File.join(File.dirname(__FILE__), '../resque_schedule.yml'))
