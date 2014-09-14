0f12e54659ca33684913c7b7952f7ba4a8e48201
Alessandro Rodi <alessandro.rodi@renuo.ch>
2014-09-14 12:16:58 +0200

    fix travis ci

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema.define(version: 20140814123057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
+  enable_extension "unaccent"
 
   create_table "action_abilitations", force: true do |t|
     t.integer  "group_action_id"
