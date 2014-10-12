9168b28beac0b5864a3f241c00114fc1638a0cac
Alessandro Rodi <alessandro.rodi@renuo.ch>
2014-10-12 19:46:48 +0200

    layout and css fixes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,2 @@
 # Be sure to restart your server when you modify this file.
-
-Airesis::Application.config.session_store :cookie_store, key: '_airesis_session', domain: :all
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# DemocracyOnline3::Application.config.session_store :active_record_store
+Airesis::Application.config.session_store :active_record_store, key: '_airesis_session', domain: :all
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140913210139) do
+ActiveRecord::Schema.define(version: 20141012160210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
 
   create_table "action_abilitations", force: true do |t|
     t.integer  "group_action_id"
@@ -1215,6 +1214,16 @@ ActiveRecord::Schema.define(version: 20140913210139) do
     t.string   "email"
     t.text     "stack"
   end
+
+  create_table "sessions", force: true do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "simple_votes", force: true do |t|
     t.integer  "candidate_id",             null: false
