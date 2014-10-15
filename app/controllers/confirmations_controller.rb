881d7cace4fec7fd445b2d6d3708aac98b7ee72b
Alessandro Rodi <alessandro.rodi@renuo.ch>
2014-10-15 15:30:52 +0200

    login the user immediately after email confirmation

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,5 +1,18 @@
 class ConfirmationsController < Devise::ConfirmationsController
 
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      set_flash_message(:notice, :confirmed) if is_flashing_format?
+      sign_in(resource) # <= THIS LINE ADDED
+      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+    else
+      respond_with_navigational(resource.errors, :status => :unprocessable_entity){ render :new }
+    end
+  end
+  
   private
 
   def after_confirmation_path_for(resource_name, resource)
