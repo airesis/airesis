20d3742389f3ef01bf29b9ba8d3adfb560879ff4
Alessandro Rodi <alessandro.rodi@renuo.ch>
2014-10-25 17:46:34 +0200

    few changes in css and homepage. updated translations

--- app/assets/javascripts/landing/main.js
+++ app/assets/javascripts/landing/main.js
@@ -164,40 +164,4 @@
         animationSpeed: 600,
         randomize: false
     });
-
-
-    /*----------------------------------------------------*/
-    /* ImageLightbox
-     /*----------------------------------------------------*/
-
-    if ($("html").hasClass('cssanimations')) {
-
-        var activityIndicatorOn = function () {
-                $('<div id="imagelightbox-loading"><div></div></div>').appendTo('body');
-            },
-            activityIndicatorOff = function () {
-                $('#imagelightbox-loading').remove();
-            },
-
-            overlayOn = function () {
-                $('<div id="imagelightbox-overlay"></div>').appendTo('body');
-            },
-            overlayOff = function () {
-                $('#imagelightbox-overlay').remove();
-            },
-
-            captionOn = function () {
-                var description = $('a[href="' + $('#imagelightbox').attr('src') + '"] img').attr('alt');
-                if (description.length > 0)
-                    $('<div id="imagelightbox-caption">' + description + '</div>').appendTo('body');
-            },
-            captionOff = function () {
-                $('#imagelightbox-caption').remove();
-            };
-    }
-    else {
-
-    }
-
-
 })(jQuery);
