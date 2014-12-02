5c00670f5ed874b8a94a480dec36fc174aa405fb
Alessandro Rodi <alessandro.rodi@renuo.ch>
2014-12-03 00:07:56 +0100

    split migrations

--- db/migrate/20140814123059_remove_blog_image_url_3.rb
+++ db/migrate/20140814123059_remove_blog_image_url_3.rb
@@ -1,6 +1,6 @@
-class RemoveBlogImageUrl2 < ActiveRecord::Migration
+class RemoveBlogImageUrl3 < ActiveRecord::Migration
   def change
-    User.where('users.id >= 2000 and users.id < 8000').where.not(blog_image_url: nil).order(:id).find_each do |user|
+    User.where('users.id >= 8000 and users.id < 12000').where.not(blog_image_url: nil).order(:id).find_each do |user|
       Rails.logger.info "Parsing #{user.blog_image_url}"
       puts "Parsing #{user.id}"
       user.avatar_url = user.blog_image_url
