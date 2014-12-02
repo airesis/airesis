42e7c723fc72242d9ea3faeaa090aabf8d4ed93a
Alessandro Rodi <alessandro.rodi@renuo.ch>
2014-12-02 23:48:49 +0100

    split migration into two parts

--- db/migrate/20140814123057_remove_blog_image_url_2.rb
+++ db/migrate/20140814123057_remove_blog_image_url_2.rb
@@ -1,6 +1,6 @@
 class RemoveBlogImageUrl < ActiveRecord::Migration
   def change
-    User.where('users.id < 2000').where.not(blog_image_url: nil).order(:id).find_each do |user|
+    User.where('users.id >= 2000').where.not(blog_image_url: nil).order(:id).find_each do |user|
       Rails.logger.info "Parsing #{user.blog_image_url}"
       user.avatar_url = user.blog_image_url
       user.save
