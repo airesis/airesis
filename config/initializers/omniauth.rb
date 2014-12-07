80f4c1f98d43944bace2cb7ab9623c71c317974b
Alessandro Rodi <alessandro.rodi@renuo.ch>
2014-12-07 11:49:15 +0100

    try another fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,12 +2,16 @@
 #  provider :facebook,  "242345195791486", "effb2e9b6fb07ce738452c8b3c1a0f57"
 #end
 
-unless Rails.env.development?
-  OmniAuth.config.full_host = Proc.new do |env|
-    uri = URI.parse(request.url)
-    uri.path = ''
-    uri.query = nil
-    uri.port = (uri.scheme == 'https' ? 443 : 80)
-    uri.to_s
+if Rails.env.production?
+  module OmniAuth
+    module Strategy
+      def full_host
+        uri = URI.parse(request.url)
+        uri.path = ''
+        uri.query = nil
+        uri.port = (uri.scheme == 'https' ? 443 : 80)
+        uri.to_s
+      end
+    end
   end
 end
