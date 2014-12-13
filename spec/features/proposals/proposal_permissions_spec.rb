354078329ba3105e6668fc1cc41642d1cde15fcb
Alessandro Rodi <alessandro.rodi@renuo.ch>
2014-12-13 17:21:30 +0100

    more tests for proposals list

--- spec/features/proposals/proposal_permissions_spec.rb
+++ spec/features/proposals/proposal_permissions_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'requests_helper'
 require "cancan/matchers"
 
-describe "check user permissions on proposals", type: :feature do
+describe "check user permissions on proposals", type: :feature, js: true, search: true do
 
   def create_proposal_in_area(visible_outside=true)
     user = create(:user)
@@ -31,10 +31,196 @@ describe "check user permissions on proposals", type: :feature do
       area = create(:group_area, group: group)
       proposal = create(:group_proposal, quorum: BestQuorum.public.first, current_user_id: user.id, group_proposals: [GroupProposal.new(group: group)], group_area_id: area.id, visible_outside: true)
       participant = create(:user)
-      create_participation(participant,group)
+      create_participation(participant, group)
       expect(Ability.new(user)).to be_able_to(:show, proposal)
       expect(Ability.new(user)).to_not be_able_to(:participate, proposal)
     end
+  end
+
+  context "proposals in lists" do
+    context "a visible_outside proposal" do
+      it "is displayed in the open space list and in the group list" do
+        user = create(:user)
+        group = create(:group, current_user_id: user.id)
+        proposal = create(:group_proposal, quorum: BestQuorum.public.first, current_user_id: user.id, group_proposals: [GroupProposal.new(group: group)])
+        visit proposals_path
+        expect(page).to have_content proposal.title
+
+        visit group_proposals_path(group)
+        expect(page).to have_content proposal.title
+      end
+
+      it "is displayed in the open space list, group list and group area list" do
+        user = create(:user)
+        group = create(:group, current_user_id: user.id)
+        area = create(:group_area, group: group)
+        proposal = create(:group_proposal, quorum: BestQuorum.public.first, current_user_id: user.id, group_proposals: [GroupProposal.new(group: group)], group_area_id: area.id, visible_outside: true)
+
+        visit proposals_path
+        expect(page).to have_content proposal.title
+
+        visit group_proposals_path(group)
+        expect(page).to have_content proposal.title
+
+        visit group_proposals_path(group, group_area_id: area.id)
+        expect(page).to have_content proposal.title
+      end
+
+      it "is not displayed in another group area list of the same group" do
+        user = create(:user)
+        group = create(:group, current_user_id: user.id)
+        area = create(:group_area, group: group)
+        area2 = create(:group_area, group: group)
+        proposal = create(:group_proposal, quorum: BestQuorum.public.first, current_user_id: user.id, group_proposals: [GroupProposal.new(group: group)], group_area_id: area.id, visible_outside: true)
+
+        visit group_proposals_path(group, group_area_id: area2.id)
+        expect(page).to_not have_content proposal.title
+      end
+
+      it "is not displayed in another group (group area) list" do
+        user = create(:user)
+        group = create(:group, current_user_id: user.id)
+        area = create(:group_area, group: group)
+        proposal = create(:group_proposal, quorum: BestQuorum.public.first, current_user_id: user.id, group_proposals: [GroupProposal.new(group: create(:group, current_user_id: create(:user).id))], visible_outside: true)
+
+        visit group_proposals_path(group)
+        expect(page).to_not have_content proposal.title
+
+        visit group_proposals_path(group, group_area_id: area.id)
+        expect(page).to_not have_content proposal.title
+      end
+    end
+
+    context "a public proposal" do
+      it "is displayed in the open space list" do
+        public_proposal = create(:public_proposal, quorum: BestQuorum.public.first, current_user_id: create(:user).id)
+        visit proposals_path
+        expect(page).to have_content public_proposal.title
+      end
+
+      it "is not displayed in a group list" do
+        proposal = create(:public_proposal, quorum: BestQuorum.public.first, current_user_id: create(:user).id)
+        user = create(:user)
+        group = create(:group, current_user_id: user.id)
+
+        visit group_proposals_path(group)
+        expect(page).to_not have_content proposal.title
+      end
+
+      it "is not displayed in a group area list" do
+        proposal = create(:public_proposal, quorum: BestQuorum.public.first, current_user_id: create(:user).id)
+        user = create(:user)
+        group = create(:group, current_user_id: user.id)
+        area = create(:group_area, group: group)
+
+        visit group_proposals_path(group, group_area_id: area.id)
+        expect(page).to_not have_content proposal.title
+      end
+    end
+
+    context "a private proposal in a group" do
+      it "is not displayed in the open space list" do
+        user = create(:user)
+        group = create(:group, current_user_id: user.id)
+        proposal = create(:group_proposal, quorum: BestQuorum.public.first, current_user_id: user.id, group_proposals: [GroupProposal.new(group: group)], visible_outside: false)
+        visit proposals_path
+        expect(page).to_not have_content proposal.title
+
+        login_as user, scope: :user
+
+        visit proposals_path
+        expect(page).to_not have_content proposal.title
+
+        logout user
+      end
+
+      it "is displayed in the group list only if you are logged in" do
+        user = create(:user)
+        group = create(:group, current_user_id: user.id)
+        proposal = create(:group_proposal, quorum: BestQuorum.public.first, current_user_id: user.id, group_proposals: [GroupProposal.new(group: group)], visible_outside: false)
+        visit group_proposals_path(group)
+        expect(page).to_not have_content proposal.title
+
+        login_as user, scope: :user
+
+        visit group_proposals_path(group)
+        expect(page).to have_content proposal.title
+
+        logout user
+      end
+
+      it "is not displayed in a group area list" do
+        user = create(:user)
+        group = create(:group, current_user_id: user.id)
+        area = create(:group_area, group: group)
+        proposal = create(:group_proposal, quorum: BestQuorum.public.first, current_user_id: user.id, group_proposals: [GroupProposal.new(group: group)], visible_outside: false)
+
+        visit group_proposals_path(group, group_area_id: area.id)
+        expect(page).to_not have_content proposal.title
+
+        login_as user, scope: :user
+
+        visit group_proposals_path(group, group_area_id: area.id)
+        expect(page).to_not have_content proposal.title
+
+        logout user
+      end
+    end
+
+    context "a private proposal in a group area" do
+      it "is not displayed in the open space list" do
+        user = create(:user)
+        group = create(:group, current_user_id: user.id)
+        area = create(:group_area, group: group)
+        proposal = create(:group_proposal, quorum: BestQuorum.public.first, current_user_id: user.id, group_proposals: [GroupProposal.new(group: group)], group_area_id: area.id, visible_outside: false)
+
+        visit proposals_path
+        expect(page).to_not have_content proposal.title
+
+        #todo test for participants in the area
+      end
+
+      it "is displayed in the group list only if you are logged in and have the permission to see it" do
+        user = create(:user)
+        group = create(:group, current_user_id: user.id)
+        area = create(:group_area, group: group)
+        proposal = create(:group_proposal, quorum: BestQuorum.public.first, current_user_id: user.id, group_proposals: [GroupProposal.new(group: group)], group_area_id: area.id, visible_outside: false)
+
+        visit group_proposals_path(group)
+        expect(page).to_not have_content proposal.title
+
+        login_as user, scope: :user
+
+        visit group_proposals_path(group)
+        expect(page).to have_content proposal.title
+
+        logout user
+
+        #todo test for participants in the area
+      end
+
+      it "is displayed in the group area list only if you are logged in and have the permission to see it" do
+        user = create(:user)
+        group = create(:group, current_user_id: user.id)
+        area = create(:group_area, group: group)
+        proposal = create(:group_proposal, quorum: BestQuorum.public.first, current_user_id: user.id, group_proposals: [GroupProposal.new(group: group)], group_area_id: area.id, visible_outside: false)
+
+        visit group_proposals_path(group, group_area_id: area.id)
+        expect(page).to_not have_content proposal.title
+
+        login_as user, scope: :user
+
+        visit group_proposals_path(group, group_area_id: area.id)
+        expect(page).to have_content proposal.title
+
+        logout user
+
+        #todo test for participants in the area
+      end
+    end
+
+
+
   end
 
   context "owner of the group permissions" do
@@ -94,7 +280,6 @@ describe "check user permissions on proposals", type: :feature do
       expect(page).to have_content(I18n.t('error.error_302.title'))
       @ability.should_not be_able_to(:show, @proposal)
     end
-
 
 
     it "can view public proposals in others group areas but can't participate" do
