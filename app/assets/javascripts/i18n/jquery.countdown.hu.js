3189b1c22689b2053c8b00207eb15d952b1a4d6d
root <root@mag.webhome.hu>
2015-01-09 22:43:55 +0100

    hungarian localisation - basics

---
+++ app/assets/javascripts/i18n/jquery.countdown.hu.js
@@ -0,0 +1,10 @@
+﻿(function($) {
+	$.countdown.regionalOptions['en'] = {
+		labels: ['Years','Months','Weeks','Days','Hours','Minutes','Seconds'],
+		labels1: ['Year','Month','Week','Day','Hour','Minute','Second'],
+		compactLabels: ['y','m','w','d'],
+		whichLabels: null,
+		digits: ['0','1','2','3','4','5','6','7','8','9'],
+		timeSeparator: ':', isRTL: false};
+	$.countdown.setDefaults($.countdown.regionalOptions['en']);
+})(jQuery);
