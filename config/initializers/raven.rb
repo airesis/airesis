bd58a87f0a17435736e3e36ff600f24700f628cd
root <root@mag.webhome.hu>
2015-01-15 15:48:47 +0100

    raven config fixed

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -2,6 +2,6 @@ if ENV['SENTRY_PUBLIC_KEY'].present?
   require 'raven'
 
   Raven.configure(false) do |config|
-    config.dsn = "https://#{ENV['SENTRY_PUBLIC_KEY']}:#{ENV['SENTRY_PRIVATE_KEY']}@app.getsentry.com/#{ENV['SENTRY_PORT']}"
+    config.dsn = "https://#{ENV['SENTRY_PUBLIC_KEY']}:#{ENV['SENTRY_PRIVATE_KEY']}@#{ENV['SENTRY_HOST']}:#{ENV['SENTRY_PORT']}/#{ENV['SENTRY_PROJECT_ID']}"
   end
 end
