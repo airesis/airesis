bdf330ab54e5683d36ac845b716599851c33dbd5
Alessandro Rodi <alessandro.rodi@renuo.ch>
2015-01-23 23:40:43 +0100

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150123190329) do
+ActiveRecord::Schema.define(version: 20150123190330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
 
   create_table "action_abilitations", force: true do |t|
     t.integer  "group_action_id"
