205bb114a16dcfbd838a7ac9a3761b0a2c26bdb8
Alessandro Rodi <alessandro.rodi@renuo.ch>
2015-03-14 15:52:00 +0100

    switch all remaining urls to https

--- app/models/proposal_nickname.rb
+++ app/models/proposal_nickname.rb
@@ -8,7 +8,7 @@ class ProposalNickname < ActiveRecord::Base
   attr_accessor :generated
 
   def avatar(size=24)
-    "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.nickname)}?s=#{size}&d=identicon&r=PG"
+    "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.nickname)}?s=#{size}&d=identicon&r=PG"
   end
 
   #a factory for nicknames. to be called when is necessary to generate a nickname for a user in a proposal
