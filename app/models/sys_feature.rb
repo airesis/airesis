28be6dcc3e5f72d37543fab64f7b49d5ec9ac26a
Alessandro Rodi <alessandro.rodi@renuo.ch>
2015-03-18 18:28:00 +0100

    fix

--- app/models/sys_feature.rb
+++ app/models/sys_feature.rb
@@ -5,7 +5,7 @@ class SysFeature < ActiveRecord::Base
   has_attached_file :image,
                     styles: {
                         medium: "300x300>"
-                    }
+                    },
                     path: "sys_features/:id/:style/:basename.:extension"
 
   validates_attachment_content_type :image, content_type: ['image/jpeg', 'image/png', 'image/gif']
