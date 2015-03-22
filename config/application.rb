0757542545f28f086f545a7787ce7026acf844a0
Alessandro Rodi <alessandro.rodi@renuo.ch>
2015-03-22 15:50:30 +0100

    move was host configuration

--- config/application.rb
+++ config/application.rb
@@ -62,7 +62,7 @@ module Airesis
             access_key_id: ENV['AWS_ACCESS_KEY_ID'],
             secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
         },
-        s3_host_name: 's3-eu-west-1.amazonaws.com'
+        s3_host_name: ENV['AWS_HOST']
     }
   end
 end
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,8 +1,5 @@
 require "paperclip"
     
-Paperclip.options[:command_path] = 'C:\Program Files\ImageMagick-6.7.2-Q16'
-Paperclip.options[:swallow_stderr] = false
-
 #todo remove in 4.1
 require 'paperclip/media_type_spoof_detector'
 module Paperclip
