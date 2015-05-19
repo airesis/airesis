ec94623eff2f8f343153d991f4f4a8d11d9d382a
Alessandro Rodi <alessandro.rodi@renuo.ch>
2015-05-19 18:19:44 +0200

    introduce greece, new zealand, england, australia and south africa

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 #encoding: utf-8
 module ApplicationHelper
-  
+
   #ricarica i messaggi flash
   def reload_flash
     page.replace "flash_messages", partial: 'layouts/flash', locals: {flash: flash}
   end
-  
+
   def javascript(*args)
     content_for(:head) { javascript_include_tag(*args) }
   end
@@ -13,20 +13,20 @@ module ApplicationHelper
   def facebook_like
     "<div class=\"fb-like\" data-send=\"false\" data-layout=\"box_count\" data-width=\"100\" data-show-faces=\"false\"></div>"
   end
-  
+
   def calendar(*args)
     "<div id='calendar'></div>"
   end
 
 
- def resource_name
+  def resource_name
     :user
   end
- 
+
   def resource
     @resource ||= User.new
   end
- 
+
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
@@ -42,10 +42,10 @@ module ApplicationHelper
           ret = I18n.l(from_time, format: :weekday) #this week
         end
       else
-        ret = I18n.l(from_time, format: :short)  #another week
+        ret = I18n.l(from_time, format: :short) #another week
       end
     elsif diff > 1.hours
-        ret = I18n.l(from_time, format: :hour)
+      ret = I18n.l(from_time, format: :hour)
     else
       ret = "<div data-countdown data-time='#{(from_time).to_i * 1000}' style='display:inline'></div>".html_safe
     end
@@ -59,4 +59,7 @@ module ApplicationHelper
     image_tag(url, alt: 'Google Authenticator QRCode')
   end
 
+  def flag_for(title, key)
+    link_to (image_tag "flags/#{key}.png", alt: title, class: 'flag-icon'), current_url(l: key), title: title
+  end
 end
---
+++ lib/geonames/parser.rb
@@ -0,0 +1,71 @@
+require 'json'
+require 'open-uri'
+require 'active_support/core_ext/hash/slice'
+require 'active_support/core_ext/hash/indifferent_access'
+
+module Geonames
+  class Parser
+
+    def initialize
+      # correctly set these parameters
+      @continent_name = 'Oceania'
+      @country_name = 'Australia'
+      @country_code = '2077456' #2635167
+      @lang = 'en'
+
+      @username = 'coorasse'
+
+      @continent_output = "continent = Continente.find_by(description: '#{@continent_name}')"
+      @country_output = "state = Stato.find_by(description: '#{@country_name}')"
+
+      @regions_output = "region = Regione.create(description: \"%{name}\", stato: state, continente: continent, geoname_id: %{geoname_id})"
+      @provinces_output = "provincia = region.provincias.create(description: \"%{name}\", stato: state, continente: continent,  geoname_id: %{geoname_id}, population: %{population})"
+      @cities_output = "provincia.comunes.create(description: \"%{name}\", regione: region, stato: state, continente: continent, geoname_id: %{geoname_id}, population: %{population})"
+    end
+
+    def extract_features(geoname)
+      {name: geoname[:name],
+       longitude: geoname[:lng],
+       latitude: geoname[:lat],
+       population: geoname[:population],
+       geoname_id: geoname[:geonameId]}
+    end
+
+    def geoname_url(code)
+      "http://api.geonames.org/childrenJSON?geonameId=#{code}&username=#{@username}&lang=#{@lang}"
+    end
+
+    def fetch(code)
+      JSON.load(open(geoname_url(code))).with_indifferent_access
+    end
+
+    def fetch_and_extract(code)
+      json = fetch(code)
+      json['geonames'] ? json['geonames'].map { |geoname| extract_features(geoname) } : []
+    end
+
+    def extract
+      country_json = fetch(@country_code)
+
+      output = File.open("#{@country_name.downcase}.rb", 'w') do |f|
+        f.puts @continent_output
+        f.puts @country_output
+        regions = fetch_and_extract(@country_code)
+
+        regions.each do |region|
+          f.puts @regions_output % region
+          provinces = fetch_and_extract(region[:geoname_id])
+          provinces.each do |provincia|
+            f.puts @provinces_output % provincia
+            cities = provinces = fetch_and_extract(provincia[:geoname_id])
+            cities.each do |city|
+              f.puts @cities_output % city
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+Geonames::Parser.new.extract
