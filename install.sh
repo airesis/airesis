330a5540f24165c08a93b9e0ea34ebad9d21e881
Alessandro Rodi <alessandro.rodi@renuo.ch>
2015-05-29 08:31:15 +0200

    install script idea

---
+++ install.sh
@@ -0,0 +1,73 @@
+echo "          _               _"
+echo "     /\\   (_)             (_)"
+echo "    /  \\   _ _ __ ___  ___ _ ___"
+echo "   / /\\ \\ | | '__/ _ \\/ __| / __|"
+echo "  / ____ \\| | | |  __/\\__ \\ \\__ \\"
+echo " /_/    \\_\\_|_|  \\___||___/_|___/"
+echo "           Scegli di partecipare"
+echo ""
+echo "=====Welcome in Airesis Setup====="
+echo "=================================="
+echo ""
+echo "We'll ask you some information to help you configure your Airesis Development Environment."
+echo "We will ask you some questions where you have to choose and identify default option with round parethesis."
+echo "Something like: 'Press Y or (N)'."
+echo "If you press y or Y will be yes, if you press n, N or Enter will be no."
+echo "We suggest you to keep default configuration if that is your first configuration."
+echo "We suggest you to not try this if you don't know how a RoR App works."
+echo "Press ENTER when you are ready to proceed"
+read
+echo ""
+echo "We will now create the database with initial data inside it. Please wait, it will take some time...and if you are wondering, NO. IT'S NOT STUCKED!"
+echo "Press ENTER when you are ready to proceed and take a coffee"
+read
+
+      sh "rake db:setup"
+      sh "mkdir -p private/elfinder"
+      Timeout::timeout(1) do
+	      STDIN.gets
+      end rescue nil
+	
+      dbconfig = YAML::load(File.open('config/database.yml'))
+      ActiveRecord::Base.establish_connection(dbconfig["development"])
+
+      require "#{File.dirname(__FILE__)}/../../config/environment.rb"
+
+      puts ""
+      puts ""
+      puts "Well done! Now some questions!"
+      puts "Would you like to activate Social Network Login on your environment?"
+      puts "You must have a different Social Network Application registered on different services and a public and private key for each one of them"
+      print "Press Y or (N)..."
+      
+      choose = STDIN.gets
+      puts choose
+      if "y" == choose.chomp.downcase
+        Configuration.find_by_name(::Configuration::SOCIALNETWORK).update_attribute(:value,1)
+        puts "Please enter your applications private and public key in config/environment.rb file"
+        print "Press ENTER when you are ready to proceed"
+        STDIN.gets
+      else
+        Configuration.find_by_name(::Configuration::SOCIALNETWORK).update_attribute(:value,0)
+      end
+      puts ""
+      puts "Would you like to activate Recaptcha when the user register?"
+      puts "If you are just developing that's not necessary"
+      print "Press Y or (N)..."
+      choose = STDIN.gets
+      if "y" == choose.chomp.downcase
+        ::Configuration.find_by_name(::Configuration::RECAPTCHA).update_attribute(:value,1)
+        puts "Please enter your recaptcha private and public key in config/environment.rb file"
+        print "Press ENTER when you are ready to proceed"
+        STDIN.gets
+      else
+        ::Configuration.find_by_name(::Configuration::RECAPTCHA).update_attribute(:value,0)
+      end
+
+      puts ""
+      puts "OK! Well done! We have finished! Now just start your Airesis environment with rails s and if something goes wrong please blame the developer."
+      puts "If you want to change other options please edit '#{environmentfilename}'"
+      puts "See ya!"
+
+    end
+end
--- prepare.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-bundle exec rake1.9 assets:precompile
-cp -r app/assets/stylesheets/redmond/images/ public/assets/
-ln -s public/assets/images/users app/assets/images
