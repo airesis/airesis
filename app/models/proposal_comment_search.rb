ae0dcfc48b6bff6d54a9ab291253c67450984762
Alessandro Rodi <alessandro.rodi@renuo.ch>
2015-05-29 17:47:31 +0200

    fix the ordering of contributes. refactoring using arel

--- app/models/proposal_comment_search.rb
+++ app/models/proposal_comment_search.rb
@@ -1,128 +1,101 @@
-class ProposalComment < ActiveRecord::Base
-  include BlogKitModelHelper
-  include LogicalDeleteHelper
-  include ActionView::Helpers::TextHelper
+class ProposalCommentSearch
 
-  has_paper_trail class_name: 'ProposalCommentVersion', only: [:content], on: [:update, :destroy]
-
-
-  belongs_to :user, class_name: 'User', foreign_key: :user_id
-  belongs_to :contribute, class_name: 'ProposalComment', foreign_key: :parent_proposal_comment_id
-  has_many :replies, class_name: 'ProposalComment', foreign_key: :parent_proposal_comment_id, dependent: :destroy
-  has_many :repliers, -> { uniq true }, class_name: 'User', through: :replies, source: :user
-  belongs_to :proposal, class_name: 'Proposal', foreign_key: :proposal_id, counter_cache: true
-  has_many :rankings, class_name: 'ProposalCommentRanking', dependent: :destroy
-  has_many :rankers, through: :rankings, class_name: 'User', source: :user
-  belongs_to :paragraph
-
-  has_one :integrated_contribute, class_name: 'IntegratedContribute', dependent: :destroy
-  has_many :proposal_revisions, class_name: 'ProposalRevision', through: :integrated_contributes
-
-  has_many :reports, class_name: 'ProposalCommentReport', foreign_key: :proposal_comment_id
-
-  validates_length_of :content, minimum: 10, maximum: CONTRIBUTE_MAX_LENGTH
-
-  attr_accessor :collapsed, :nickname_generated
-
-  after_initialize :set_collapsed
-
-  validate :check_last_comment
-
-  scope :contributes, -> { where(['parent_proposal_comment_id is null']) }
-  scope :comments, -> { where(['parent_proposal_comment_id is not null']) }
-
-  scope :unintegrated, -> { where(integrated: false) }
-  scope :integrated, -> { where(integrated: true) }
-
-  scope :noise, -> { where(noise: true) }
-
-  scope :listable, -> { where({integrated: false, noise: false}) }
-
-  scope :unread, lambda { |user_id, proposal_id| where(["proposal_comments.id not in (select p2.id from proposal_comments p2 join proposal_comment_rankings pr on p2.id = pr.proposal_comment_id where pr.user_id = ? and p2.proposal_id = ?) ", user_id, proposal_id]) }
-
-  scope :removable, -> { where(['soft_reports_count >= ? and noise = false', CONTRIBUTE_MARKS]) }
-
-  #a contribute marked more than three times as spam
-  scope :spam, -> { where(['grave_reports_count >= ?', CONTRIBUTE_MARKS]) }
-
-  #a contribute marked more than three times as noisy
-  scope :noisy, -> { where(['soft_reports_count >= ?', CONTRIBUTE_MARKS]) }
-
-  attr_accessor :section_id
-
-  before_create :set_paragraph_id
-
-  after_create :generate_nickname
-  after_commit :send_email, on: :create
-  after_commit :send_update_notifications, on: :update
-
-  def is_contribute?
-    self.parent_proposal_comment_id.nil?
+  def initialize(params, proposal, current_user = nil)
+    @proposal = proposal
+    @all = params[:all]
+    @view = params[:view]
+    @page = params[:page]
+    @comment_id = params[:comment_id].to_i
+    @section_id = params[:section_id]
+    @section = Section.find(@section_id) if @section_id.present?
+    @contributes = params[:contributes]
+    @limit = params[:disable_limit] ? 9999999 : COMMENTS_PER_PAGE
+    @current_user = current_user
   end
 
-  def set_paragraph_id
-    self.paragraph = Paragraph.where(section_id: self.section_id).first
+  def random_order?
+    @view == SearchProposal::ORDER_RANDOM
   end
 
-  def set_collapsed
-    @collapsed = false
+  def rank_order?
+    @view == SearchProposal::ORDER_BY_RANK
   end
 
-  def check_last_comment
-    comments = self.proposal.proposal_comments.where(user_id: self.user_id).order("created_at DESC")
-    comment = comments.first
-    if LIMIT_COMMENTS and comment and ((Time.now - comment.created_at) < 30.seconds)
-      self.errors.add(:created_at, "devono passare almeno trenta secondi tra un commento e l'altro.")
+  def show_more?
+    puts "total: #{@total_pages}"
+    puts "current: #{@current_page}"
+    @total_pages > @current_page
+  end
+
+  # return a list of ids of already evaluated contributes by the current_user
+  def evaluated_ids
+    return @evaluated_ids if @evaluated_ids
+    if @current_user
+      @evaluated_ids = ProposalComment.joins(:rankings).
+        where(proposal_comment_rankings: {user_id: @current_user.id},
+              proposal_comments: {proposal_id: @proposal.id}).uniq.pluck(:id)
+    else
+      @evaluated_ids = []
     end
+    @evaluated_ids.delete(@comment_id) if @comment_id
+    @evaluated_ids
   end
 
-  # Used to set more tracking for akismet
-  def request=(request)
-    self.user_ip = request.remote_ip
-    self.user_agent = request.env['HTTP_USER_AGENT']
-    self.referrer = truncate(request.env['HTTP_REFERER'], length: 255)
+  def order_clause
+    'random()' unless @comment_id
+    "CASE proposal_comments.id
+      WHEN #{@comment_id.to_i} THEN 1 ELSE 5 END, random()"
   end
 
-  #retrieve all the participants to this discussion
-  def participants
-    self.repliers | [self.user]
-  end
+  def proposal_comments
+    return @proposal_comments if @proposal_comments
 
-  def has_location?
-    !self.paragraph.nil?
-  end
+    proposal_comments_t = ProposalComment.arel_table
 
-  def location
-    ret = nil
-    unless self.paragraph.nil?
-      section = self.paragraph.section
-      ret = "#{section.title}"
-      unless section.solution.nil?
-        ret = "#{section.solution.title_with_seq} > #{ret}"
+    if @section.present?
+      paragraphs_ids = @section.paragraph_ids
+      conditions_arel = proposal_comments_t[:paragraph_id].in(paragraphs_ids)
+    elsif @all.present?
+      conditions_arel = 1.eq(1)
+    else
+      conditions_arel = proposal_comments_t[:paragraph_id].eq(nil)
+    end
+
+    if random_order?
+
+      order = order_clause
+
+      #remove already shown contributes
+      conditions_arel = conditions_arel.and(proposal_comments_t[:id].not_in @contributes) if @contributes
+      left = @limit
+      tmp_comments = []
+
+      # extract not evaluated contributes
+      tmp_comments += @proposal.contributes.listable.
+        where(conditions_arel.and(proposal_comments_t[:id].not_in(evaluated_ids))).
+        order(order).take(left)
+
+      left -= tmp_comments.count
+
+      if left > 0 && !evaluated_ids.empty?
+        # extract the evaluated ones
+        tmp_comments += @proposal.contributes.listable.
+          where(conditions_arel.and(proposal_comments_t[:id].in(evaluated_ids))).
+          order('rank desc').take(left)
       end
+      @proposal_comments = tmp_comments
+      @total_pages = (@proposal.contributes.listable.where(conditions_arel).count.to_f / COMMENTS_PER_PAGE.to_f).ceil
+      @current_page = (@page || 1).to_i
+    else
+      order = if rank_order?
+                [proposal_comments_t[:j_value].desc, proposal_comments_t[:id].desc]
+              else
+                proposal_comments_t[:updated_at].desc
+              end
+      @proposal_comments = @proposal.contributes.listable.where(conditions_arel).order(order).page(@page).per(@limit)
+      @total_pages = @proposal_comments.total_pages
+      @current_page = @proposal_comments.current_page
     end
-    ret
-  end
-
-  def send_email
-    NotificationProposalCommentCreate.perform_async(id)
-  end
-
-
-  def send_update_notifications
-    if previous_changes.include?(:content) && previous_changes[:content].first != previous_changes[:content].last
-      NotificationProposalCommentUpdate.perform_async(id)
-    end
-  end
-
-  def generate_nickname
-    proposal_nickname = ProposalNickname.generate(user, proposal)
-    self.nickname_generated = proposal_nickname.generated
-  end
-
-  #unintegrate the comment if it was integrated somewhere
-  def unintegrate
-    integrated_contribute.destroy
-    update(integrated: false)
+    @proposal_comments
   end
 end
