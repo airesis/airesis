ffd6f9102aebceb77780cf94a9e7fadffe4d29d4
Alessandro Rodi <coorasse@gmail.com>
2015-07-19 15:13:38 +0200

    fix statistics visualization on firefox

--- app/models/stat_num_proposal.rb
+++ app/models/stat_num_proposal.rb
@@ -1,7 +1,7 @@
 class StatNumProposal < ActiveRecord::Base
 
   def self.extract
-    self.connection.select_all "select  year || '-' || month   as date, sum(value)
+    self.connection.select_all "select  year || '-' || month || '-01'   as date, sum(value)
             from stat_num_proposals
             group by year, month
             having (year != #{Date.today.year} or month != #{Date.today.month})
