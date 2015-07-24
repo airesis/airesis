c5a96117b81d360489c8156dafee425f901f2adc
Alessandro Rodi <coorasse@gmail.com>
2015-07-24 12:30:40 +0200

    ! update translations!

--- lib/crowdin/client.rb
+++ lib/crowdin/client.rb
@@ -5,128 +5,133 @@ require 'zip'
 module Crowdin
   class Client
 
-    def initialize
+    def initialize(params={})
+      @extract_folder = params[:extract_folder] || 'tmp/crowdin'
+      @download_folder = params[:download_folder] || 'tmp'
+      @min_translation_percentage = params[:min_translation_percentage] || 20
+      @locales_mapping = {'crowdin' => 'en-GB'}
       auth
     end
 
-    DOWNLOAD_FOLDER = "tmp"
-    MIN_TRANSLATION_PERCENTAGE = 60
-    FAKE_LANGUAGE = "en-GB"
-
     def auth
-      @crowdin = Crowdin::API.new(api_key: ENV['CROWDIN_API'] , project_id: ENV['CROWDIN_PROJECT_ID'], account_key: ENV['CROWDIN_ACCOUNT_KEY'])
-      @crowdin.log = Logger.new $stderr
+      @crowdin = Crowdin::API.new(api_key: ENV['CROWDIN_API'],
+                                  project_id: ENV['CROWDIN_PROJECT_ID'],
+                                  account_key: ENV['CROWDIN_ACCOUNT_KEY'])
+      #@crowdin.log = Logger.new $stderr
     end
 
     def upload_sources
-      source_files_path = Dir["config/locales/**/*.en.yml"]
+      source_files_path = Dir['config/locales/**/*.en.yml']
 
       files=[]
       source_files_path.each { |path|
-        files << { dest: "/#{File.basename(path)}", source: path, export_pattern: "/"+path.gsub("en.yml","%locale%.yml")}
+        files << {dest: "/#{File.basename(path)}",
+                  source: path,
+                  export_pattern: '/'+path.gsub('en.yml', '%locale%.yml')}
       }
-      @crowdin.add_file(files, :type=>'yaml')
+      @crowdin.add_file(files, :type => 'yaml')
     end
 
     def upload_translations
-      transl_files_path = Dir["config/locales/**/*.it-IT.yml"]
+      transl_files_path = Dir['config/locales/**/*.it-IT.yml']
       files = []
       transl_files_path.each { |path|
-        files << { dest: "/#{File.basename(path).gsub(/(?<=\.)(.*)(?=\.yml)/, "en")}", source: path }
+        files << {dest: "/#{File.basename(path).gsub(/(?<=\.)(.*)(?=\.yml)/, 'en')}", source: path}
       }
 
       @crowdin.upload_translation(
-          files,
-          language = 'it',
-           params = {import_duplicates: true})
+        files,
+        language = 'it',
+        params = {import_duplicates: true})
     end
 
     #scan directory "locales", memorize the names of the directories inside it
     #passes the directories name to crowdin.update_files, that upload the english files inside each directory
     def update_sources
-      source_files_path = Dir["config/locales/**/*.en.yml"]
+      source_files_path = Dir['config/locales/**/*.en.yml']
       files=[]
 
       source_files_path.each { |path|
-        files << { dest: "/#{File.basename(path)}", source: path}
-        }
+        files << {dest: "/#{File.basename(path)}", source: path}
+      }
       @crowdin.update_file(files)
     end
 
 
-    #check translation_status
-    #build zip file in Crowdin server
-    #download only zip files of languages inside @lang_ready (see #status for more info on @lang_ready)
+    # check translation_status
+    # build zip file in Crowdin server
+    # download only zip files of languages inside @lang_ready (see #status for more info on @lang_ready)
     def download_translations
-      self.status
+      status
       @crowdin.export_translations
       @lang_ready.each { |lang|
-        @crowdin.download_translation(lang, output: "#{DOWNLOAD_FOLDER}/output-#{lang}.zip")
+        puts "Downloading '#{lang}' in zip format"
+        @crowdin.download_translation(lang, output: "#{@download_folder}/output-#{lang}.zip")
       }
     end
 
 
-    #extract the zip file of each language contained in the folder "config/locales", only if the files in the archive are not empty
-    #delete zip files at the end of extraction
-    #reload translations from file (if there are new .yml files added we still need to restart the application!)
+    # extract the zip file of each language contained in the folder "config/locales",
+    # only if the files in the archive are not empty
+    # delete zip files at the end of extraction
+    # reload translations from file (if there are new .yml files added we still need to restart the application!)
     def extract_zip
-      zip_files = Dir["#{DOWNLOAD_FOLDER}/*.zip"]
+      FileUtils.rmtree(@extract_folder)
+      zip_files = Dir["#{@download_folder}/*.zip"]
 
-      zip_files.each{ |zip|
+      zip_files.each { |zip|
         Zip::File.open(zip) { |zip_file|
           zip_file.each { |f|
             file_name = f.name
-            FileUtils.mkdir_p(File.dirname(file_name))
+            FileUtils.mkdir_p("#{@extract_folder}/#{File.dirname(file_name)}")
 
-            if (file_name.include? "#{FAKE_LANGUAGE}") && !(file_name.include? "assets")
-              zip_file.rename(f,file_name.gsub(/(?<=\.)(.*)(?=\.)/, "crowdin") )
-            end
-
-            if f.size && f.size != 0 && !(f.name.include? "#{FAKE_LANGUAGE}") #doesn't extract empty files or files with fake locale
-              zip_file.extract(f, f.name) { true } #if true overwrite existing files with same name
+            # doesn't extract empty files or files with fake locale
+            if f.size && f.size != 0
+              # if true overwrite existing files with same name
+              zip_file.extract(f, "#{@extract_folder}/#{f.name}") { true }
             end
           }
         }
         delete_zip(zip)
       }
       self.change_fakelocale
-      I18n.reload!
     end
 
+    # set a fake locale for crowdin files
     def change_fakelocale
-      files = Dir["config/locales/*/*.crowdin.yml"]
-      files.each{ |file_name|
-        File.open("tmpfile", 'w') { |tmp|
-          File.open(file_name, 'r').each { |l|
-            if l.chomp == "#{FAKE_LANGUAGE}:"
-              tmp << "crowdin:\n"
-            else
-              tmp << l
-            end
+      @locales_mapping.each do |key, value|
+        puts "Converting '#{key}' into #{value}"
+        files = Dir["#{@extract_folder}/config/locales/*/*.#{key}.yml"]
+        files.each { |file_name|
+          File.open('tmpfile', 'w') { |tmp|
+            File.open(file_name, 'r').each { |l|
+              if l.chomp == "#{key}:"
+                tmp << "#{value}:\n"
+              else
+                tmp << l
+              end
+            }
+            FileUtils.mv(tmp.path, file_name)
           }
-          FileUtils.mv(tmp.path, file_name)
         }
-      }
+      end
     end
 
     def delete_zip(zip_file)
       File.delete(zip_file)
     end
 
-    #check the status of the translations and populate the array @lang_ready with the lang code
-    #that have translation percentage superior to MIN_TRANSLATION_PERCENTAGE
+    # check the status of the translations and populate the array @lang_ready with the lang code
+    # that have translation percentage superior to @min_translation_percentage
     def status
-      auth
       @lang_ready= []
 
-      output_array =  @crowdin.translations_status
+      output_array = @crowdin.translations_status
       output_array.each do |lang|
-      next if lang["translated_progress"] < MIN_TRANSLATION_PERCENTAGE
-          @lang_ready << lang["code"]
+        next if lang['translated_progress'] < @min_translation_percentage
+        @lang_ready << lang['code']
       end
-      @lang_ready << "#{FAKE_LANGUAGE}" unless @lang_ready.include?("#{FAKE_LANGUAGE}")
+      puts "Available languages: #{@lang_ready}"
     end
-
-
   end
 end
