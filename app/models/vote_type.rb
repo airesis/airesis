ed7ae241bc4227095980dd4feedd69870619340b
Alessandro Rodi <coorasse@gmail.com>
2015-08-21 22:39:39 +0200

    remove commented lines

--- app/models/vote_type.rb
+++ app/models/vote_type.rb
@@ -1,9 +1,4 @@
 class VoteType < ActiveRecord::Base
-  # translates :description
-
-  # has_many :proposal_comment_rankings, class_name: 'ProposalCommentRanking'
-  # has_many :proposal_rankings, class_name: 'ProposalRanking'
-
   POSITIVE = 1
   NEUTRAL = 2
   NEGATIVE = 3
