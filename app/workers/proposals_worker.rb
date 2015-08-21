4db733d470c9099af10ee82638310acb2df2675b
Alessandro Rodi <coorasse@gmail.com>
2015-08-21 23:01:38 +0200

    don't use instance variable when is not necessaey

--- app/workers/proposals_worker.rb
+++ app/workers/proposals_worker.rb
@@ -10,33 +10,33 @@ class ProposalsWorker
 
   def perform(*args)
     params = args[0]
-    @proposal = Proposal.lock.find(params['proposal_id'])
+    proposal = Proposal.lock.find(params['proposal_id'])
     case params['action']
     when ENDTIME
       # fa terminare la fase di valutazione di una proposta
-      @proposal.check_phase
+      proposal.check_phase
     when LEFT24
       # invia una notifica a tutti i partecipanti alla proposta
       # che non hanno dato la loro valutazione o possono cambiarla
       # 24 ore prima della chiusura del dibattito
-      NotificationProposalTimeLeft.perform_async(@proposal.id, '24_hours')
+      NotificationProposalTimeLeft.perform_async(proposal.id, '24_hours')
     when LEFT1
       # invia una notifica a tutti i partecipanti alla proposta
       # che non hanno dato la loro valutazione o possono cambiarla
       # 1 ora prima della chiusura del dibattito
-      NotificationProposalTimeLeft.perform_async(@proposal.id, '1_hour')
+      NotificationProposalTimeLeft.perform_async(proposal.id, '1_hour')
     when LEFT24VOTE
       # send a notification to all participants that can vote the proposal and haven't voted it yet
       # 24 ore prima della chiusura del dibattito
-      NotificationProposalTimeLeftVote.perform_async(@proposal.id, '24_hours_vote')
+      NotificationProposalTimeLeftVote.perform_async(proposal.id, '24_hours_vote')
     when LEFT1VOTE
       # send a notification to all participants that can vote the proposal and haven't voted it yet
       # 1 ora prima della chiusura del dibattito
-      NotificationProposalTimeLeftVote.perform_async(@proposal.id, '1_hour_vote')
+      NotificationProposalTimeLeftVote.perform_async(proposal.id, '1_hour_vote')
     else
       puts '==Action not found!=='
     end
-  rescue ActiveRecord::RecordNotFound => e
+  rescue ActiveRecord::RecordNotFound
     logger.warn "can't find proposal #{params['proposal_id']}. Has it been deleted?"
   end
 end
