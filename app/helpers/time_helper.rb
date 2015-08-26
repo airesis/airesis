09a91dd57cc95e1b73eb86703e79ab68d4b9a294
Alexander Belov <git@belov.by>
2015-08-26 21:50:55 +0300

    #1451 Remove old calendar images Clear up events.css.scss Use parentheses when defining a method in according to style guide

--- app/helpers/time_helper.rb
+++ app/helpers/time_helper.rb
@@ -1,9 +1,9 @@
 module TimeHelper
-  def month_long month
+  def month_long(month)
     t('calendar.monthNames')[month.to_i - 1]
   end
 
-  def month_short month
+  def month_short(month)
     t('calendar.monthNamesShort')[month.to_i - 1]
   end
 end
