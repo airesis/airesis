d866eb0086ac2d1cba36ad9852bd23cefdc20ae9
Alessandro Rodi <coorasse@gmail.com>
2015-11-27 17:42:10 +0100

    remove a lot of unused stuff

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Airesis::Application.routes.draw do
 
   resources :sys_payment_notifications, only: [:create]
 
-  resources :user_likes
+  resources :user_likes, only: [:create, :destroy]
 
   resources :proposal_nicknames, only: [:update]
 
@@ -113,15 +113,12 @@ Airesis::Application.routes.draw do
     post :hide, on: :member
   end
 
-  resources :tutorial_progresses
-
-  resources :tutorials do
-    resources :steps do
+  resources :tutorials, only: [] do
+    resources :steps, only: [] do
       member do
         get :complete
       end
     end
-    resources :tutorial_assignees
   end
 
   resources :alerts do
@@ -539,7 +536,7 @@ Airesis::Application.routes.draw do
           get :download_translations
           get :extract_delete_zip
         end
-        resources :tutorials
+
         resources :users, only: [] do
           get :unblock, on: :member
           collection do
