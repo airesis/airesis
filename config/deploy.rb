2e26930ebb46c751e3d805e257d97d90939a7acc
Alessandro Rodi <coorasse@gmail.com>
2016-04-06 09:26:12 +0200

    fix issue with new rvm signature key

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,3 +26,10 @@ set :default_shell, 'bash -l'
 set :bundle_flags, '--deployment'
 
 before 'deploy', 'rvm1:install:ruby'
+
+namespace :app do
+  task :update_rvm_key do
+    execute :gpg, '--keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3'
+  end
+end
+before 'rvm1:install:rvm', 'app:update_rvm_key'
