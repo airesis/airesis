946a1116f4ed2379950eb69d10d82fd2d2d34899
Alessandro Rodi <alessandro.rodi@renuo.ch>
2019-09-16 17:55:51 +0200

    fix cookie consent banner (#161)

--- app/assets/javascripts/cookies_eu.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('.cookies-eu-ok').click(function (e) {
-        e.preventDefault();
-        Cookie.set('cookie_eu_consented', 'true', {path: '/', expires: 365});
-        $('.cookies-eu').remove();
-    });
-});
